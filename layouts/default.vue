2405ea75948f04f06c9e1b5fad7ee1c8736624cf
Ian Germann <ian@shoebox.md>
2019-10-24 22:26:33 -0400

    Remove some default components/styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <div class='top-bar'>
+      <h1>Ian Germann</h1>
+    </div>
     <nuxt />
   </div>
 </template>
@@ -24,32 +27,14 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.top-bar {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-weight: 300;
+  font-size: 2rem;
+  letter-spacing: 1px;
+  display: flex;
+  justify-content: center;
+  border-bottom: 3px solid black;
 }
 </style>
