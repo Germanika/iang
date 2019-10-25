99e541186e985c3cc476b78eb13159b2a3b9d837
Ian Germann <Germanika@users.noreply.github.com>
2019-10-24 23:38:33 -0400

    Add basic contact info (#3)

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="container">
+    <About />
+    <Contact />
   </div>
 </template>
 
+<script>
+import Contact from '../components/contact'
+import About from '../components/about'
+
+export default {
+  components: {
+    Contact,
+    About
+  }
+}
+</script>
+
 <style>
 .container {
-  margin: 0 auto;
+  margin: 2rem;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
 }
-
 </style>
